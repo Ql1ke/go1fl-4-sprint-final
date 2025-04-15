5adeeb560ca77e380bf9370bd43b68a2c08531f6
Sergey Kulik <sgkulik@avito.ru>
2025-04-15 10:20:49 +0300

    Add logic in func.parsePackage

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,6 +1,9 @@
 package daysteps
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -11,8 +14,28 @@ const (
 	mInKm = 1000
 )
 
+// parsePackage принимает строку вида "678,0h50m",
+// где "678" - количество шагов, а "0h50m" - продолжительность прогулки
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	comma := strings.Split(data, ",")
+	if len(comma) != 2 {
+		return 0, 0, fmt.Errorf("неверный формат данных, ожидается 'steps, duration', получили: %s", data)
+	}
+
+	steps, err := strconv.Atoi(comma[0])
+	if err != nil {
+		return 0, 0, fmt.Errorf("ошибка преобразования количества шагов (%s): %w", comma[0], err)
+	}
+	if steps <= 0 {
+		return 0, 0, fmt.Errorf("количество шагов должно быть > 0, получено: %d", steps)
+	}
+
+	duration, err := time.ParseDuration(comma[1])
+	if err != nil {
+		return 0, 0, fmt.Errorf("ошибка преоборзаования продолжительности (%s): %w", comma[1], err)
+	}
+
+	return steps, duration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
